e57e45f1bf1ad7de5d8869f48b0499b973070cf1
cx75510 <cx75510@gmail.com>
2017-05-29 09:01:13 +0900

    DB initialize & UserDao 구현 _ 완료

---
+++ qna/src/main/java/net/slipp/dao/users/UserDaoTest.java
@@ -0,0 +1,39 @@
+package net.slipp.dao.users;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import net.slipp.domain.users.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/applicationContext.xml")
+public class UserDaoTest {
+	private static final Logger logger = LoggerFactory.getLogger(UserDaoTest.class);
+	
+	@Autowired
+	private UserDao userDao;
+
+	@Test
+	public void findById() {
+		User user = userDao.findById("javajigi");
+		logger.debug("User : {}", user);
+	}
+	
+	@Test
+	public void create() throws Exception{
+		User user = new User("sanjigi", "1234", "sanjigi", "san@san");
+		userDao.create(user);
+		User actual = userDao.findById(user.getUserId());
+		
+		assertThat(actual, is(user));
+	}
+
+}
