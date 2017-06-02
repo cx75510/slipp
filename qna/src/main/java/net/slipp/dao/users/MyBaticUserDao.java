1afdebd04d577551c1422403bc8d7f651ef1af35
cx75510 <cx75510@gmail.com>
2017-06-02 16:14:34 +0900

    flyway Database Migration Tools 적용 _ 완료

--- qna/src/main/java/net/slipp/dao/users/MyBaticUserDao.java
+++ qna/src/main/java/net/slipp/dao/users/MyBaticUserDao.java
@@ -1,39 +1,17 @@
 package net.slipp.dao.users;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import net.slipp.domain.users.User;
 
 public class MyBaticUserDao implements UserDAO {
-	private static final Logger logger = LoggerFactory.getLogger(MyBaticUserDao.class);
 	private SqlSession sqlSession;
-	
-	private DataSource dataSource;
-	
-	@PostConstruct
-	public void initialze(){
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("slipp.sql"));
-		DatabasePopulatorUtils.execute(populator, dataSource);
-		logger.info("database initialized success!");
-	}
 	
 	public void setSqlSession(SqlSession sqlSession) {
 		this.sqlSession = sqlSession;
 	}
 	
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
-
 	@Override
 	public User findById(String userId) {
 		return sqlSession.selectOne("UserMapper.findById",userId);
